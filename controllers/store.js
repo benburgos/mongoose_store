e6102073262f2bbb8631e70311e2c1b78f3633a9
Ben <ben.a.burgos@gmail.com>
2022-04-17 20:38:49 -0700

    adds controllers

--- controllers/store.js
+++ controllers/store.js
@@ -5,40 +5,47 @@ const productRouter = express.Router();
 
 // Routes /////////////////////////////////////////////////
 // Index Route
-productRouter.get('/store', (req, res) => {
+productRouter.get('/', (req, res) => {
     res.send(`You're at the main page!`)
     console.log('--Index page accessed--')
 });
 
 // Seed Route
-productRouter.get('/store/seed', (req, res) => {console.log('--Data was seeded!--')});
+productRouter.get('/seed', (req, res) => {
+    console.log('--Data was seeded!--')
+    Product.deleteMany({}, (err, deletedProducts) => {
+        Product.create(productSeedData, (err, data) => {
+            res.redirect('/store')
+        });
+    });
+});
 
 // New Route
-productRouter.get('/store/new', (req, res) => {
+productRouter.get('/new', (req, res) => {
     res.send(`You're at the page to create a new item!`)
     console.log('--New item form page accessed--')
 });
 
 // Create Route
-productRouter.post('/store', (req, res) => {
+productRouter.post('/', (req, res) => {
     res.send(`You sent in a new item!`)
     console.log(`--Create route was hit from the NEW page--`)
 });
 
 // Show Route
-productRouter.get('/store/:id', (req, res) => {
+productRouter.get('/:id', (req, res) => {
     res.send(`You're look at item details for ${req.params.id} here!`)
     console.log(`--Show route was accessed!`)
 });
 
 // Edit Route
-productRouter.get('/store/:id/edit', (req, res) => {
+productRouter.get('/:id/edit', (req, res) => {
     res.send(`You're editing item ${req.params.id} here!`)
     console.log(`--Edit route was hit!--`);
 });
 
 // Update Route
-productRouter.put('/store/:id', (req, res) => {
+productRouter.put('/:id', (req, res) => {
     res.send(`You sent in an update request for ${req.params.id}!`)
     console.log(`--Update route was hit from the EDIT page--`)
 });
