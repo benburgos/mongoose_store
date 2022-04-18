82d3c953ae880a1f35c1903f871007c8c584a2e4
Ben <ben.a.burgos@gmail.com>
2022-04-17 19:42:13 -0700

    adds init index route

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 require('dotenv').config();
 
-// Dependencies
+// Dependencies ///////////////////////////////////////////
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 
-// Listener
+// Request Logger /////////////////////////////////////////
+const morgan = require('morgan');
+app.use(morgan('tiny'));
+
+// Middleware /////////////////////////////////////////////
+app.use(express.urlencoded({extended: false}));
+app.use(express.static('public'));
+
+// Routes /////////////////////////////////////////////////
+// Index Route
+app.get('/store', (req, res) => {
+    res.send(`You're at the main page!`)
+});
+// New Route
+// Create Route
+// Show Route
+// Edit Route
+// Update Route
+// Delete Route
+
+// Listener ///////////////////////////////////////////////
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`You're live on port ${PORT}!`));
